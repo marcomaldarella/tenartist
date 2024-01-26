0638719c77d0fa788ce2927e371e5a51176e297a
Marco Maldarella <marcomaldarella@gmail.com>
2024-01-26 02:31:10 +0100

    pulizia

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ import Home from './Components/Home/Home';
 import About from './Components/About/About';
 import Projects from './Components/Projects/Projects';
 import Project from './Components/Project/Project';
-import Careers from './Components/Careers/Careers';
 import Contact from './Components/Contact/Contact';
 import './App.css';
 
@@ -27,7 +26,6 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/projects' element={<Projects />} /> 
           <Route path='/about' element={<About />} />
-          <Route path='/careers' element={<Careers />} />
           <Route path='/contact' element={<Contact />} />
           <Route path='/sample-project-page' element={<Project />} />
         </Routes>

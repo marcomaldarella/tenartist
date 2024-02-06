abb1ee8ea4cb05e735b7a8e13cba1f157fa824cf
Marco Maldarella <marcomaldarella@gmail.com>
2024-02-06 10:38:16 +0100

    debuh project

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,11 +1,10 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useNavigate, useParams } from 'react-router-dom';
 import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
-import transition from "../transition";
 import ProjectNavbar from './ProjectNavbar';
 import ProjectFooter from './ProjectFooter';
 import "./Project.css";
-import projectData from '../Projects/Projects.json'; 
+import projectData from '../Projects/Projects.json';
 
 const Project = () => {
   const navigate = useNavigate();
@@ -14,20 +13,51 @@ const Project = () => {
   const [currentProject, setCurrentProject] = useState(null);
 
   useEffect(() => {
-    // Forzare l'aggiornamento di Locomotive Scroll all'aggiornamento del componente
-    document.dispatchEvent(new Event('DOMContentLoaded'));
-  }, [currentProject]);
-
-  const maxImagesPerProject = 8; // Numero massimo di immagini per progetto
-
-  useEffect(() => {
     const projectIndex = projectData.findIndex(p => p.slug === projectSlug);
     if (projectIndex !== -1) {
       setCurrentProject(projectData[projectIndex]);
     } else {
-      // Qui puoi gestire il caso in cui il progetto non viene trovato
+      console.log('Project not found');
     }
   }, [projectSlug]);
+
+  useEffect(() => {
+    const projectScrollHandler = () => {
+      const container = containerRef.current;
+      const totalWidth = container.scrollWidth - container.offsetWidth;
+
+      if (container.scrollLeft >= totalWidth) {
+        // Se si raggiunge la fine, si riparte dall'inizio (con un piccolo offset per evitare un loop immediato)
+        container.scrollTo({ left: 1, behavior: 'smooth' });
+      } else if (container.scrollLeft <= 0) {
+        // Se si raggiunge l'inizio, si va verso la fine (meno un piccolo offset)
+        container.scrollTo({ left: totalWidth - 1, behavior: 'smooth' });
+      }
+    };
+
+    const container = containerRef.current;
+    container.addEventListener('scroll', projectScrollHandler);
+
+    // Aggiungi un listener per ridimensionare la pagina quando la finestra del browser cambia dimensioni
+    const resizeHandler = () => {
+      if (container) {
+        // Calcola la larghezza totale in base alle dimensioni della finestra del browser
+        const totalWidth = window.innerWidth * projectData.length;
+        container.style.width = `${totalWidth}px`;
+      }
+    };
+
+    // Esegui il listener di ridimensionamento iniziale
+    resizeHandler();
+
+    // Aggiungi il listener di ridimensionamento
+    window.addEventListener('resize', resizeHandler);
+
+    return () => {
+      container.removeEventListener('scroll', projectScrollHandler);
+      window.removeEventListener('resize', resizeHandler);
+    };
+  }, [currentProject]);
 
   const handleNextPreviousProject = (direction) => {
     const currentIndex = projectData.findIndex(p => p.slug === projectSlug);
@@ -41,15 +71,7 @@ const Project = () => {
 
   const getProjectImages = (projectId) => {
     const basePath = `/assets/${projectId}/`;
-    let images = [];
-    for (let i = 1; i <= maxImagesPerProject; i++) {
-      images.push(`${basePath}${i}.jpg`);
-    }
-    return images;
-  };
-
-  const handleImageError = (e) => {
-    e.target.style.display = 'none';
+    return Array.from({ length: 8 }, (_, i) => `${basePath}${i + 1}.jpg`);
   };
 
   const renderProjectContent = () => {
@@ -57,17 +79,18 @@ const Project = () => {
       return (
         <video width="100%" height="auto" controls>
           <source src={currentProject.videoPath} type="video/mp4" />
-          Il tuo browser non supporta il tag video.
+          Your browser does not support the video tag.
         </video>
       );
     } else {
-      return getProjectImages(currentProject.id).map((imagePath, index) => (
+      const images = getProjectImages(currentProject.id);
+      return images.map((imagePath, index) => (
         <img 
           key={index} 
           src={imagePath} 
           alt={`Project Image ${index}`} 
           className="project-image"
-          onError={handleImageError}
+          onError={(e) => e.target.style.display = 'none'}
         />
       ));
     }
@@ -78,10 +101,9 @@ const Project = () => {
       options={{
         smooth: true,
         direction: 'horizontal',
-
         smartphone: {
           direction: 'horizontal',
-          smooth: true
+          smooth: true,
         },
         tablet: {
           direction: 'horizontal',
@@ -91,25 +113,25 @@ const Project = () => {
       watch={[currentProject]}
       containerRef={containerRef}
     >
-      <ProjectNavbar 
-        onNext={() => handleNextPreviousProject("next")} 
-        onPrevious={() => handleNextPreviousProject("prev")} 
-      />
       <div data-scroll-container ref={containerRef} className="content">
-        {
-         currentProject && (
-          <div data-scroll-section>{renderProjectContent()}</div>
-         )
-        }
-      </div>
-      {currentProject && (
-        <ProjectFooter 
-          name={currentProject.name} 
-          category={currentProject.category}
+        <ProjectNavbar 
+          onNext={() => handleNextPreviousProject("next")} 
+          onPrevious={() => handleNextPreviousProject("prev")} 
         />
-      )}
+        {currentProject && (
+          <div data-scroll-section>
+            {renderProjectContent()}
+          </div>
+        )}
+        {currentProject && (
+          <ProjectFooter 
+            name={currentProject.name} 
+            category={currentProject.category}
+          />
+        )}
+      </div>
     </LocomotiveScrollProvider>
   );
 };
 
-export default transition(Project);
+export default Project;

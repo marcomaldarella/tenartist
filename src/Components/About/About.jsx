a4a4e21e672a858371b8697d2f80e8d4e2b0bf42
Your Name <you@example.com>
2024-03-20 18:19:17 +0100

    rechecka

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,77 +1,53 @@
 import React, { useRef } from "react";
-
-import { LocomotiveScrollProvider } from "react-locomotive-scroll";
-
 import transition from "../transition";
-
 import "./About.css";
 
 const About = () => {
-  const containerRef = useRef(null);
-
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        smartphone: {
-          smooth: true,
-        },
-        tablet: {
-          smooth: true,
-        },
-      }}
-      watch={[]}
-      containerRef={containerRef}
-    >
-      <div
-        className="about"
-        data-scroll-container
-        ref={containerRef}
-        id="scroll-container"
-      >
-        <div className="about-copy">
-          <p className="copy-about-margins">TEN ARTIST IS A PRODUCTION COMPANY SPECIALIZED IN PRINT AND VIDEO CAMPAIGNS, DIGITAL AND SOCIAL CONTENTS, WE ARE BASED IN MILAN BUT WE TAKE CARE OF PROJECTS EVERYWHERE IN ITALY AND EUROPE. TEN ARTIST HAS MORE THAN 10 YEARS OF EXPERIENCE. WE ORGANIZE PHOTO AND VIDEO SHOOTINGS, STARTING FROM THE VERY BEGINNING WITH THE CREATIVE BRIEF, UNTIL THE FINALIZATION AND DELIVERY OF THE FINAL IMAGES, READY TO BE PUBLISHED. WE COLLABORATE WITH AMAZING CLIENTS, BIG PHOTOGRAPHERS, GREAT STYLIST EITHER FOR COMMERCIAL OR EDITORIAL PROJECTS. WE WORK IN STUDIO AS IN PRIVATE LOCATION OF ALL SORT, OUTDOOR IN THE CITY AS IN THE WILD NATURE.
-          </p>
-          <br />
-          <br />
-          <span className="about-copy-2" >
-             Our space, located in Milan, is available for photo shooting and events
-          </span>
+    <div className="about" data-scroll-container id="scroll-container">
+      <div className="about-copy">
+        <p className="copy-about-margins">
+          TEN ARTIST IS A PRODUCTION COMPANY SPECIALIZED IN PRINT AND VIDEO CAMPAIGNS, DIGITAL AND SOCIAL CONTENTS, WE ARE BASED IN MILAN BUT WE TAKE CARE OF PROJECTS EVERYWHERE IN ITALY AND EUROPE. TEN ARTIST HAS MORE THAN 10 YEARS OF EXPERIENCE. WE ORGANIZE PHOTO AND VIDEO SHOOTINGS, STARTING FROM THE VERY BEGINNING WITH THE CREATIVE BRIEF, UNTIL THE FINALIZATION AND DELIVERY OF THE FINAL IMAGES, READY TO BE PUBLISHED. WE COLLABORATE WITH AMAZING CLIENTS, BIG PHOTOGRAPHERS, GREAT STYLIST EITHER FOR COMMERCIAL OR EDITORIAL PROJECTS. WE WORK IN STUDIO AS IN PRIVATE LOCATION OF ALL SORT, OUTDOOR IN THE CITY AS IN THE WILD NATURE.
+        </p>
+        <br />
+        <br />
+        <span className="about-copy-2">
+          Our space, located in Milan, is available for photo shooting and events
+        </span>
 
-          <div className="image-marquee">
-            <div className="image-container">
-              <span>
-                <img src="/assets/about-hero.jpg" alt="About Hero"/>
-                <img src="/assets/about-office.jpg" alt="About Office"/>
-                <img src="/assets/about-office-2.jpg" alt="About Office"/>
-                <img src="/assets/about-studio.jpg" alt="About Office"/>
-                <img src="/assets/about-feature.jpg" alt="About Feature"/>
-                <img src="/assets/about-doors.jpg" alt="About Feature"/>
-                <img src="/assets/about-outside.jpg" alt="About Feature"/>
-              </span>
-              <span>
-                <img src="/assets/about-hero.jpg" alt="About Hero"/>
-                <img src="/assets/about-office.jpg" alt="About Office"/>
-                <img src="/assets/about-office-2.jpg" alt="About Office"/>
-                <img src="/assets/about-studio.jpg" alt="About Office"/>
-                <img src="/assets/about-feature.jpg" alt="About Feature"/>
-                <img src="/assets/about-doors.jpg" alt="About Feature"/>
-                <img src="/assets/about-outside.jpg" alt="About Feature"/>
-              </span>
-            </div>
-          </div>
-
-          <div className="marquee">
+        <div className="image-marquee">
+          <div className="image-container">
             <span>
-              Creative Direction, Budget Management, Art buying, Crew selection, Location research and scouting, Municipality permits, Casting, Logistics, Transportations, Insurance, Lighting and Equipment, Digital tech and retouching services, Props, Set design/building, Location vehicles and prop cars, Catering services
+              <img src="/assets/about-hero.jpg" alt="About Hero" />
+              <img src="/assets/about-office.jpg" alt="About Office" />
+              <img src="/assets/about-office-2.jpg" alt="About Office" />
+              <img src="/assets/about-studio.jpg" alt="About Office" />
+              <img src="/assets/about-feature.jpg" alt="About Feature" />
+              <img src="/assets/about-doors.jpg" alt="About Feature" />
+              <img src="/assets/about-outside.jpg" alt="About Feature" />
             </span>
             <span>
-              Creative Direction, Budget Management, Art buying, Crew selection, Location research and scouting, Municipality permits, Casting, Logistics, Transportations, Insurance, Lighting and Equipment, Digital tech and retouching services, Props, Set design/building, Location vehicles and prop cars, Catering services
+              <img src="/assets/about-hero.jpg" alt="About Hero" />
+              <img src="/assets/about-office.jpg" alt="About Office" />
+              <img src="/assets/about-office-2.jpg" alt="About Office" />
+              <img src="/assets/about-studio.jpg" alt="About Office" />
+              <img src="/assets/about-feature.jpg" alt="About Feature" />
+              <img src="/assets/about-doors.jpg" alt="About Feature" />
+              <img src="/assets/about-outside.jpg" alt="About Feature" />
             </span>
           </div>
         </div>
+
+        <div className="marquee">
+          <span>
+            Creative Direction, Budget Management, Art buying, Crew selection, Location research and scouting, Municipality permits, Casting, Logistics, Transportations, Insurance, Lighting and Equipment, Digital tech and retouching services, Props, Set design/building, Location vehicles and prop cars, Catering services
+          </span>
+          <span>
+            Creative Direction, Budget Management, Art buying, Crew selection, Location research and scouting, Municipality permits, Casting, Logistics, Transportations, Insurance, Lighting and Equipment, Digital tech and retouching services, Props, Set design/building, Location vehicles and prop cars, Catering services
+          </span>
+        </div>
       </div>
-    </LocomotiveScrollProvider>
+    </div>
   );
 };
 
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -1,6 +1,5 @@
 import { useRef, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
-import { LocomotiveScrollProvider } from 'react-locomotive-scroll';
 import ProjectItem from './ProjectItem';
 import transition from '../transition';
 import { projectsData } from './projectsData';
@@ -8,24 +7,12 @@ import { Link } from 'react-router-dom';
 import './Projects.css';
 
 const Projects = transition(() => {
-  const containerRef = useRef(null);
   const location = useLocation();
 
   useEffect(() => {
-    if (containerRef.current && containerRef.current.update) {
-      containerRef.current.update();
-    }
-
-    const images = document.querySelectorAll('.project-img-full-wrapper img');
-    let delay = 800; // Ritardo iniziale di 0.8 secondi in millisecondi
-
-    images.forEach((img) => {
-      setTimeout(() => {
-        img.classList.add('fade-in');
-      }, delay);
-
-      delay += 100; // Incrementiamo il ritardo per l'immagine successiva di 0.1 secondi
-    });
+    // Gestione dell'aggiornamento della pagina o azioni specifiche al cambio di location
+    // Ad esempio, scrollare verso l'alto quando la location cambia
+    window.scrollTo(0, 0);
   }, [location]);
 
   let projectElements = [];
@@ -34,7 +21,6 @@ const Projects = transition(() => {
   let standardIndex = 0, wideIndex = 0;
 
   while (standardIndex < standardProjects.length || wideIndex < wideProjects.length) {
-    // Inserisci due coppie di progetti standard
     for (let i = 0; i < 2 && standardIndex < standardProjects.length; i++) {
       projectElements.push(
         <div key={`standard-pair-${standardIndex}`} className="projects-list-standard">
@@ -53,10 +39,9 @@ const Projects = transition(() => {
           ))}
         </div>
       );
-      standardIndex += 2; // Aggiorna l'indice per la prossima coppia
+      standardIndex += 2;
     }
 
-    // Inserisci un progetto wide se disponibile
     if (wideIndex < wideProjects.length) {
       projectElements.push(
         <div key={wideProjects[wideIndex].id} className="projects-list-wide">
@@ -71,66 +56,25 @@ const Projects = transition(() => {
           </Link>
         </div>
       );
-      wideIndex++; // Passa al prossimo progetto wide
+      wideIndex++;
     }
   }
 
-  // Alla fine, se ci sono ancora progetti wide da mostrare, aggiungili tutti
-  while (wideIndex < wideProjects.length) {
-    projectElements.push(
-      <div key={wideProjects[wideIndex].id} className="projects-list-wide">
-        <Link to={`/project/${wideProjects[wideIndex].slug}`}>
-          <ProjectItem
-            name={wideProjects[wideIndex].name}
-            category={wideProjects[wideIndex].category}
-            imagePath={wideProjects[wideIndex].imagePath}
-            projectId={wideProjects[wideIndex].id}
-            isWide={true}
-          />
-        </Link>
-      </div>
-    );
-    wideIndex++; // Continua con il prossimo progetto wide
-  }
-
   return (
-    <LocomotiveScrollProvider
-      options={{
-        smooth: true,
-        direction: 'vertical',
-        smartphone: {
-          smooth: true,
-          direction: 'vertical',
-        },
-        tablet: {
-          direction: 'vertical',
-          smooth: true,
-        },
-      }}
-      watch={[]}
-      containerRef={containerRef}
-    >
-      <div className="projects-container" data-scroll-container ref={containerRef}>
-        <section className="projects" data-scroll-section>
-          {projectElements}
-        </section>
-        <section className="footer" data-scroll-section>
-          <div className="footer-copy">
-            <div className="footer-copy-text">
-              <a className="footer-links" href="mailto:info@tenartist.com">
-                info@tenartist.com
-              </a>
-              <a className="footer-links-2" href="https://www.instagram.com/ten_artist">
-                Instagram
-              </a>
-              <span className="footer-links-3">
-                @2024
-              </span>
-            </div>
+    <div className="projects-container">
+      <section className="projects">
+        {projectElements}
+      </section>
+      <section className="footer">
+        <div className="footer-copy">
+          <div className="footer-copy-text">
+            <a className="footer-links" href="mailto:info@tenartist.com">info@tenartist.com</a>
+            <a className="footer-links-2" href="https://www.instagram.com/ten_artist">Instagram</a>
+            <span className="footer-links-3">@2024</span>
           </div>
-        </section>
-      </div>
-    </LocomotiveScrollProvider>
+        </div>
+      </section>
+    </div>
   );
 });
 
